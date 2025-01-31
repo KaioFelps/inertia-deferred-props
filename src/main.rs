6bd540a76f02edb2c9680e4ea06c5b56aaa7b606
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-01-31 17:28:21 -0300

    feat: register server middlewares

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
-use actix_web::{web::Data, App, HttpServer};
+use actix_session::SessionMiddleware;
+use actix_web::{
+    cookie::{Key, SameSite},
+    web::Data,
+    App, HttpServer,
+};
+use file_session::FileSessionStore;
+use helpers::{get_env_mode, RustEnv};
+use inertia_rust::actix::InertiaMiddleware;
+use middlewares::{
+    garbage_collector::GarbageCollectorMiddleware, inertia_reflasher::ReflashTemporarySession,
+};
 use std::io;
 
 mod file_session;
@@ -13,8 +24,28 @@ async fn main() -> io::Result<()> {
     let inertia = inertia::initialize_inertia(vite).await?;
     let inertia = Data::new(inertia);
 
-    HttpServer::new(move || App::new().app_data(inertia.clone()))
-        .bind(("0.0.0.0", 3000))?
-        .run()
-        .await
+    // obviously this should be an actual key and be safely stored inside your environment variables
+    // instead of a hard-coded string slice
+    let key = Key::derive_from("voiJKNMNJvbjknjHBO86&&$(#IUKJS@1".as_bytes());
+    let storage = FileSessionStore::default();
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(inertia.clone())
+            .wrap(GarbageCollectorMiddleware)
+            .wrap(InertiaMiddleware::new())
+            .wrap(ReflashTemporarySession)
+            .wrap(
+                SessionMiddleware::builder(storage.clone(), key.clone())
+                    .cookie_domain(Some("localhost".into()))
+                    .cookie_http_only(true)
+                    .cookie_same_site(SameSite::Strict)
+                    .cookie_name("test_cookie_session_id".into())
+                    .cookie_secure(get_env_mode() == RustEnv::Production)
+                    .build(),
+            )
+    })
+    .bind(("0.0.0.0", 3000))?
+    .run()
+    .await
 }
