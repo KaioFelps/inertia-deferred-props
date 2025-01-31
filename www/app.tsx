96513de04e0fa482dce89003fb915dee4608ea00
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-01-31 15:04:48 -0300

    chore: setup inertia client

---
+++ www/app.tsx
@@ -0,0 +1,18 @@
+import { createInertiaApp } from "@inertiajs/react"
+import { createRoot } from "react-dom/client";
+
+createInertiaApp({
+    progress: { includeCSS: true, color: "#fff800" },
+    
+    title: (title: string) => title ?? "Inertia Rust",
+    
+    resolve: (pageName: string) => {
+        const pages = import.meta.glob("./pages/**/*.tsx", { eager: true });
+        const page = pages[`./page/${ pageName }.tsx`];
+        return page;
+    },
+    
+    setup: ({ App, el,props }) => {
+        createRoot(el).render(<App {...props} />)
+    },
+})
